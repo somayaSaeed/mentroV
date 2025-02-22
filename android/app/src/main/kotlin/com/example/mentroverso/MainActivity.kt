c056d118e58820302f0245f99792a83ba605d067
ToDo_App <somayasaeid75@gmail.com>
2025-02-22 19:39:15 +0200

    create the project.

---
+++ android/app/src/main/kotlin/com/example/mentroverso/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mentroverso
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(const MentroVerso());
+}
+
+class MentroVerso extends StatelessWidget {
+  const MentroVerso({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
